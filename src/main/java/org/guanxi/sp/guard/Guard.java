0494ae359fda0757d74b948152f7062f281c433c
Alistair Young <alistair@codebrane.com>
2008-06-05 11:08:21 +0000

    Fixed bug with the time parameter sent to the IdP

--- src/main/java/org/guanxi/sp/guard/Guard.java
+++ src/main/java/org/guanxi/sp/guard/Guard.java
@@ -314,7 +314,7 @@ public class Guard implements Filter {
     // The target parameter is meant to come back as is from the IdP
     wayfLocation += "?shire=" + config.getEngineInfo().getAuthConsumerURL();
     wayfLocation += "&target=" + sessionID;
-    wayfLocation += "&time=" + "sdfsdsdf";
+    wayfLocation += "&time=" + Utils.zuluNow();
     wayfLocation += "&providerId=" + config.getGuardInfo().getID();
 
     // Send the user to the WAYF or IdP
