4f0b05bcf7992d02b23a4c154eb943620e3d259c
Alistair Young <alistair@codebrane.com>
2009-10-07 10:59:26 +0000

    This commit was manufactured by cvs2svn to create branch 'targetconnect'.
    Cherrypick from master 2009-10-07 10:59:25 UTC Alistair Young <alistair@codebrane.com> 'Updated process to read the SOAP/SAML from the Engine from an explicit request parameter instead of reading from the InputStream. This fixes problems when running in Blackboard':
        src/main/webapp/guanxi_sp/index.jsp
        src/main/php/guard/headers.php
        Documentation.txt
        src/main/webapp/WEB-INF/guanxi_sp_guard/truststore/readme.txt
        src/main/webapp/WEB-INF/guanxi_sp_guard/keystore/readme.txt
        src/main/webapp/protected/headers.jsp
        src/main/php/guard/GuanxiSessionVerifierService.php
        src/main/webapp/guanxi_sp/images/formback.gif
        src/main/php/guard/.htaccess
        src/main/php/guard/messages.php
        src/main/webapp/WEB-INF/guanxi_sp_guard/config/guanxi-sp-guard.xml
        MPL-1.1.txt
        src/main/java/org/guanxi/sp/guard/AttributeConsumer.java
        src/main/java/org/guanxi/sp/guard/Util.java
        src/main/php/guard/Pod.php
        src/main/webapp/guanxi_sp/images/logo.gif
        src/main/php/guard/GuanxiAttributeConsumerService.php
        pom.xml
        src/main/php/guard/readme.txt
        src/main/php/guard/config.php
        src/main/webapp/index.html
        src/main/webapp/WEB-INF/guanxi_sp_guard/jsp/logout.jsp
        src/main/java/org/guanxi/sp/guard/Guard.java
        src/main/webapp/guanxi_sp/images/watermark.gif
        src/main/php/guard/GuanxiGuard.php
        src/main/php/guard/utils.php
        src/main/webapp/guanxi_sp/index.html
        src/main/php/guard/error.php
        src/main/webapp/WEB-INF/web.xml
        src/main/resources/log4j.properties
        src/main/webapp/WEB-INF/guanxi_sp_guard/jsp/sp_error.jsp
        readme.txt
        src/main/webapp/guanxi_sp/stylesheet/guanxi_sp.css
        src/main/php/guard/GuanxiPodderService.php
        src/main/php/guard/elgg/gx.php
    Cherrypick from guanxi-guard-2_0_1 2008-12-19 08:09:06 UTC Alistair Young <alistair@codebrane.com> 'This commit was manufactured by cvs2svn to create branch 'guanxi-guard-':
        src/main/java/org/guanxi/sp/guard/GuardRequest.java
        src/main/java/org/guanxi/sp/guard/Logout.java
        src/main/java/org/guanxi/sp/guard/Podder.java
        src/main/webapp/WEB-INF/guanxi_sp_guard/logs/readme.txt
        src/main/java/org/guanxi/sp/guard/SessionVerifier.java
        src/main/java/org/guanxi/sp/guard/gateway/spring/Gatekeeper.java

---
+++ src/main/java/org/guanxi/sp/guard/GuardRequest.java
@@ -0,0 +1,127 @@
+//: "The contents of this file are subject to the Mozilla Public License
+//: Version 1.1 (the "License"); you may not use this file except in
+//: compliance with the License. You may obtain a copy of the License at
+//: http://www.mozilla.org/MPL/
+//:
+//: Software distributed under the License is distributed on an "AS IS"
+//: basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//: License for the specific language governing rights and limitations
+//: under the License.
+//:
+//: The Original Code is Guanxi (http://www.guanxi.uhi.ac.uk).
+//:
+//: The Initial Developer of the Original Code is Alistair Young alistair@codebrane.com
+//: All Rights Reserved.
+//:
+
+package org.guanxi.sp.guard;
+
+import org.guanxi.common.Pod;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import java.util.*;
+
+/**
+ * The GuardRequest represents the Guard in the browser. It encapsulates all the original Request
+ * information and adds attributes as request headers, obtained from an IdP via en Engine.
+ *
+ * @author Alistair Young alistair@smo.uhi.ac.uk
+ */
+public class GuardRequest extends HttpServletRequestWrapper {
+  Pod requestPod = null;
+  Hashtable<String, String> headers = null;
+  Cookie[] cookies = null;
+
+  @SuppressWarnings("unchecked")
+  public GuardRequest(HttpServletRequest request, Pod pod, String attributePrefix) {
+    super(request);
+
+    requestPod = pod;
+
+    headers = new Hashtable<String, String>();
+
+    cookies = new Cookie[super.getCookies().length];
+    cookies = super.getCookies();
+
+    String buffer = null;
+    Enumeration<String> requestHeaderNames = super.getHeaderNames();
+    while (requestHeaderNames.hasMoreElements()) {
+      buffer = (String)requestHeaderNames.nextElement();
+      // Servlet spec states that header names are case insensitive
+      headers.put(buffer.toLowerCase(), super.getHeader(buffer));
+    }
+
+    // Make sure there are attributes to process
+    if (pod.getBag().hasAttributes()) {
+      Enumeration<String> samlAttributeNames = pod.getBag().getAttributeNames();
+      while (samlAttributeNames.hasMoreElements()) {
+        buffer = (String)samlAttributeNames.nextElement();
+        headers.put(attributePrefix + buffer, pod.getBag().getAttributeValue(buffer));
+      }
+    }
+  }
+
+  public String getParameter(String name) {
+    /* The original parameter values are stored as String[] no matter how
+     * many of them there are. i.e. a param with one value will still
+     * return a String[] for that value.
+     */
+    String[] values = (String[])requestPod.getRequestParameters().get(name);
+
+    // Return the first value of the param
+    return (values != null) ? values[0] : null;
+  }
+
+  public Map<?, ?> getParameterMap() {
+    return requestPod.getRequestParameters();
+  }
+
+  public Enumeration<?> getParameterNames() {
+    return new Enumeration<Object>() {
+      Iterator<?> names = requestPod.getRequestParameters().keySet().iterator();
+
+      public boolean hasMoreElements() {
+        return names.hasNext();
+      }
+
+      public Object nextElement() {
+        return names.next();
+      }
+    };
+  }
+
+  public Enumeration<?> getAttributeNames() {
+    return super.getAttributeNames();
+  }
+
+  public Object getAttribute(String name) {
+    return super.getAttribute(name);
+  }
+
+  public void setAttribute(String name, Object value) {
+    super.setAttribute(name, value);
+  }
+
+  public String[] getParameterValues(String name) {
+    // See note on getParameter
+    return (String[])requestPod.getRequestParameters().get(name);
+  }
+
+  public String getHeader(String name) {
+    // Look for the header name as is. If it's not there, try all lower case
+    return (headers.get(name) != null) ? (String)headers.get(name) : (String)headers.get(name.toLowerCase());
+  }
+
+  public Enumeration<String> getHeaderNames() {
+    return headers.keys();
+  }
+
+  public Enumeration<?> getHeaders(String name) {
+    return super.getHeaders(name);
+  }
+
+  public Cookie[] getCookies() {
+    return cookies;
+  }
+}
