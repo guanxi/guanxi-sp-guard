a8131b0627f5ee53ed57105fa27a2bc6bf18eaa3
Alistair Young <alistair@codebrane.com>
2010-01-15 11:27:33 +0000

    Added logging. Added postProcessGetGuardId, getLogoutMessageAttributeName, getLogoutSuccessMessage, getLogoutErrorMessage, getLogoutResource (targetconnect)

--- src/main/java/org/guanxi/sp/guard/Logout.java
+++ src/main/java/org/guanxi/sp/guard/Logout.java
@@ -16,6 +16,8 @@
 
 package org.guanxi.sp.guard;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.guanxi.common.Pod;
 import org.guanxi.common.definitions.Guanxi;
 import org.guanxi.common.filters.FileName;
@@ -33,10 +35,14 @@ import java.io.IOException;
  * This servlet provides an HTTP binding for logout functionality.
  * It calls the low level Guard API to do the actual logout.
  *
- * @author Alistair Young alistair@smo.uhi.ac.uk
+ * @author alistair
+ * @author chris
  */
 @SuppressWarnings("serial")
 public class Logout extends HttpServlet {
+
+	private Log logger = LogFactory.getLog(getClass());
+
   public void init() throws ServletException {
   }
 
@@ -60,26 +66,59 @@ public class Logout extends HttpServlet {
    */
   public void processLogout(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     org.guanxi.xal.sp.GuardDocument.Guard config = (org.guanxi.xal.sp.GuardDocument.Guard)getServletContext().getAttribute(Guanxi.CONTEXT_ATTR_GUARD_CONFIG);
-    String cookieName = config.getCookie().getPrefix() + FileName.encode(config.getGuardInfo().getID());
-    
+    String cookieName = config.getCookie().getPrefix() + FileName.encode(postProcessGetGuardId(config.getGuardInfo().getID(),request));
+
     boolean loggedOut = false;
     Cookie[] cookies = request.getCookies();
+
+    logger.debug("processLogout: attempting to find cookie: " + cookieName);
+
     if (cookies != null) {
       for (int c = 0; c < cookies.length; c++) {
         if (cookies[c].getName().equals(cookieName)) {
+
+        	logger.debug("processLogout: found cookie: " + cookieName);
           Pod pod = (Pod)getServletContext().getAttribute(cookies[c].getValue());
           if (pod != null) {
+        	  logger.debug("processLogout: deactivating pod for session: " + pod.getSessionID());
             Guard.deactivatePod(pod);
             loggedOut = true;
-            request.setAttribute("LOGOUT_MESSAGE", "You have successfully logged out of the SP");
+            request.setAttribute(getLogoutMessageAttributeName(), getLogoutSuccessMessage());
           }
         }
       }
     }
 
     if (!loggedOut)
-      request.setAttribute("LOGOUT_MESSAGE", "You have not successfully logged out of the SP");
+      request.setAttribute(getLogoutMessageAttributeName(), getLogoutErrorMessage());
 
-    request.getRequestDispatcher("/WEB-INF/guanxi_sp_guard/jsp/logout.jsp").forward(request, response);
+    request.getRequestDispatcher(getLogoutResource()).forward(request, response);
+  }
+
+  /**
+   * Opportunity for extending filters to dynamically control the guard id
+   *
+   * @param id The current Guard ID
+   * @param httpRequest Servlet request
+   * @return The new Guard ID. Could be the same as the current one
+   */
+  protected String postProcessGetGuardId(String id, HttpServletRequest httpRequest) {
+	  return id;
+  }
+
+  protected String getLogoutMessageAttributeName() {
+	  return "LOGOUT_MESSAGE";
+  }
+
+  protected String getLogoutSuccessMessage() {
+	  return "You have successfully logged out of the SP";
+  }
+
+  protected String getLogoutErrorMessage() {
+	  return "You have not successfully logged out of the SP";
+  }
+
+  protected String getLogoutResource() {
+	  return "/WEB-INF/guanxi_sp_guard/jsp/logout.jsp";
   }
 }
